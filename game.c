70853dce20703b6d43597bf32cd6802685a5f189
sanmatte <matteee.sanna@gmail.com>
2025-02-26 12:36:52 +0100

    fix pause

--- game.c
+++ game.c
@@ -359,7 +359,7 @@ int play(WINDOW *game) {
                     }
                     break;
                 case PAUSE_ID:
-                    WINDOW *pause = newwin(7, 28, (GAME_HEIGHT/2) + 2,  (GAME_WIDTH/2) + 8);
+                    WINDOW *pause = newwin(7, 28, (GAME_HEIGHT/2) + 3,  (GAME_WIDTH/2) + 8);
                     print_pause(pause, game);
                     killpg(group_pid, SIGSTOP);  // pause all child processes
                     int ch = getchar();          // wait for user input
